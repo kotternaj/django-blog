f17a30204ebdd0636d8161a5e5b7c6ce76945258
Untitled <john.d.reddock@gmail.com>
2018-06-08 15:29:45 -0400

    views

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url(r'^$',views.PostListView.as_view(),name='post_list'),
+    url(r'about/$',views.AboutView.as_view(),name='about'),
+    url(r'post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
+]
+
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from blog.models import Post,Comment
+from django.views.generic import(TemplateView,ListView,
+                                 DetailView,)
 
-# Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.object.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
